09acb3017200a8c09f48b43572953746d805a53a
Melissa Garcia <melissag@ensemble.com>
2024-06-19 14:53:21 -0700

    test 6

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         path: '/guides/'
       },
       {
-        title: 'api full page #273',
+        title: 'api full page #274',
         path: '/api-full/'
       }
     ],
