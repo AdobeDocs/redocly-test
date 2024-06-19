bb672336068bc91d8d1d296686f73f9d031929f8
Melissa Garcia <melissag@ensemble.com>
2024-06-19 10:09:12 -0700

    test pass as string - default

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         path: '/guides/'
       },
       {
-        title: 'api full page #263',
+        title: 'api full page #264',
         path: '/api-full/'
       }
     ],
