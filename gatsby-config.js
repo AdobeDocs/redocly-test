2a8714b8907e956eb1f692924a631ea0ff079532
Melissa Garcia <melissag@ensemble.com>
2024-07-19 09:01:33 -0700

    update aio thee

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         path: '/guides/'
       },
       {
-        title: 'api full page #308',
+        title: 'api full page #309',
         path: '/api-full/'
       }
     ],
